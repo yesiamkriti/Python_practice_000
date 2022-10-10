18e43e69d5d9ea336567eaf8695796e23e711380
Kriti <kritijulia@gmail.com>
2022-10-10 20:12:53 +0530

    week's day calculator

---
+++ python007.py
@@ -0,0 +1,17 @@
+# week's day calculator
+number = int(input("enter number between 1 to 7 to get the day:"))
+if number == 1:
+    print("sunday")
+elif number == 2:
+    print("monday")
+elif number == 2:
+    print("tuesday")
+elif number == 3:
+    print("wednesday")
+elif number == 4:
+    print("thrusday")
+elif number == 5:
+    print("friday")
+
+else:
+    print("enter a valid number")

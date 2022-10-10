ed9a3b7292d42ee497dac1de60385fdabb2d416b
Kriti <kritijulia@gmail.com>
2022-10-10 14:06:40 +0530

    addition and subtraction

---
+++ python002.py
@@ -0,0 +1,24 @@
+# assume to number a and b . Sum these number.
+# first way
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+sum = a + b
+print("sum of both number will :", sum)
+#second way
+x = 46
+y = 78
+z = x + y
+print("the sum is:", z)
+# assume to number c and d . subtract these number
+# first way
+q = x - y
+print("the subtraction is:" , q)
+#second way
+c = int(input("Enter third number :"))
+d = int(input("Enter fourth number:"))
+subtract = c - d
+print("the subtraction will :" , subtract)
+#third way
+def sub(A,B):
+    return(A-B)
+print(sub(90,70))

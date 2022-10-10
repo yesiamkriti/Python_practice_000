637c23d463d762cdc4b23a21f3a311e3c18e86ac
Kriti <kritijulia@gmail.com>
2022-10-10 12:58:23 +0530

    first coding in python and use of interger,float,string,boolean

---
+++ python001.py
@@ -0,0 +1,14 @@
+# it is my first coding in python .
+print("hello world")
+# let go with following data type :
+#integer
+name = 'KRITI'
+section = 26.0 
+registration_no = 12217541
+class_roll_no = 8
+female = True
+print(type(name))
+print(type(section))
+print(type(registration_no)) 
+print(type(class_roll_no))
+print(type(female))

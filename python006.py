542483d2f646fd395bfda15cf75f89018e3a378e
Kriti <kritijulia@gmail.com>
2022-10-10 20:07:52 +0530

    marks calculator on the basis of attendence

---
+++ python006.py
@@ -0,0 +1,10 @@
+#marks according to your attendence
+marks = int(input("enter your marks :"))
+if marks > 90 and mark <= 100:
+ print ("your grade is A")
+elif marks > 80 and marks <= 90:
+ print("your grade is B")
+elif marks > 70  and marks <=80:
+ print("your grade is C")
+else:
+ print(" enter a valid number")

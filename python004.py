f6b7e3c72d33de17df48c09587f17de1eb791b37
Kriti <kritijulia@gmail.com>
2022-10-10 19:24:03 +0530

    area of triangle

---
+++ python004.py
@@ -0,0 +1,12 @@
+# area of triangle by using herons formula
+a = 4
+b = 3
+c = 5
+s = (a + b + c) / 2
+A = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('area of triangle:', A)
+#area of triangle  by simple method
+base = 4
+height = 3
+area = base * height * 1/2
+print("area of triangle is :" , area)

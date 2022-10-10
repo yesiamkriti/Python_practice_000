792227d60b2b86affb0702f38ae6a567f42a1bb5
Kriti <kritijulia@gmail.com>
2022-10-10 20:21:20 +0530

    simple and shortcut way to use if,else,elif coding

---
+++ python008.py
@@ -0,0 +1,12 @@
+#simple way to use if,elif,else
+a = 3
+b = 5
+c = 9
+if a < b :
+    print("hey")
+elif a == b :
+    print ("hello")
+else :
+    print ("hi")
+# shortcut way to use if and else
+print("hello") if(a>b) else print("ABCD")

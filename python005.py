16cf1fd1bc57c7c3834afdc2428c438bad38c979
Kriti <kritijulia@gmail.com>
2022-10-10 19:49:33 +0530

    complex +,-,*,**

---
+++ python005.py
@@ -0,0 +1,18 @@
+# how to write complex number
+z = print(complex(2,5))
+# complex subtraction
+x = complex(23, 45)
+y = complex(45,43)
+def subComplex(x,y):
+    return x-y
+print("the subtraction is : " , subComplex(x,y))
+# complex addition
+def subComplex(x,y):
+    return x+y
+print("the sum is :", subComplex(x,y))
+def subComplex(x,y):
+    return x**y
+print("the power is :", subComplex(x,y))
+def subComplex(x,y):
+    return x*y
+print("the multiplication is :", subComplex(x,y))

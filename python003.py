62381df36fd2bec2c09ade0e7a0dd775708f15d5
Kriti <kritijulia@gmail.com>
2022-10-10 19:06:22 +0530

    swaping of data

---
+++ python003.py
@@ -0,0 +1,13 @@
+#take the inputs from user for two numbers (t and p)
+t = 45 
+r = 66
+print("before swap, the value of t:",t)
+print("before swap, the value of r:",r)
+p = t
+print("value of p is:",p)
+t = r
+print("value of t is:", p)
+print("value of r is:", r)
+print("after swap, the value of t is", t)
+print("after swap, the value of r is", r)
+print("after swap, the value of p is", p)

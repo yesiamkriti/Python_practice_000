42aeee150b4acf2dbccef923250e1725ae947f0b
Kriti <kritijulia@gmail.com>
2022-10-19 21:24:33 +0530

    random number,replace,length,upperandlower case

---
+++ python013.py
@@ -0,0 +1,41 @@
+#random number
+import random
+print(random.randrange(1,6))
+#array: multiple value which we can store in a variable
+string = "ABCD"
+print(string[2])
+print(len(string))
+a = "          HELLO WORLD"
+b = "Hello"
+c = "World"
+d = b + c
+print(d)
+print(a[8:11])
+print(a[-1])
+print(a[:7])
+print(a[7:])
+print(a[-4:-1])
+print(len(a))
+#upper and lowercase
+print(a.lower())
+#strip is used to remove unwanted space
+print(a)
+print(a.strip())
+#replace
+print(a.replace("HELLO","Hey"))
+#in
+Chilu = "A quick brown fox jump over the lazy dog"
+if "quick" in Chilu:
+    print("Yes")
+else:
+    print("no")
+#not in
+if "quick" not in Chilu:
+    print("Yes")
+else:
+    print("no")
+print('"classmate"')
+print('"classmate"')
+print('"classmate"')
+print('"classmate"')
+print("classmate")

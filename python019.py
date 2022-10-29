ef893d0c1983922333d61caad5da99ddf2a04b73
Kriti <kritijulia@gmail.com>
2022-10-29 22:18:24 +0530

    loop while,for,range,list uses

---
+++ python019.py
@@ -0,0 +1,31 @@
+a = 1
+while a < 10:
+    print(a)
+    a = a+1
+i = 1
+while i <= 4:
+    print("*" * i)
+    i += 1
+# write a program to print first 10 even numbers using while loop
+c = 0
+while c < 20:
+     print(c)
+     c+=2
+d = 10
+while d > 0:
+    print(d)
+    d -= 1
+cars = ["tata","BMW","Kia","Nano","ford"]
+for i in cars:
+    print(i)
+print(cars[:3])
+for j in cars:    
+    if (j == "Kia"):
+         break
+    print(j)
+fruits = ["apple","mango","papaya","pineapple"]
+flowers = ["sunflower","rose","lily","lotus"]
+for k in fruits:
+    for l in flowers:
+        print(k,l)
+for k in range      

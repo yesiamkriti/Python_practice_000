bb53386b535ee2cc6eedf14501bfb88a4d7c77bc
Kriti <kritijulia@gmail.com>
2022-11-05 08:30:13 +0530

    write a program to print the following num patterns using aloop

---
+++ python022.py
@@ -0,0 +1,11 @@
+row= int(input())
+for i in range(1,row +1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print('')
+rows = int(input())
+for k in range(rows,0,-1):
+    for l in range(1,k+1):
+        print(l,end="")
+    print("")
+    

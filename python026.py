ea3ef507c8334283d43f7e361674d6456a062bd7
Kriti <kritijulia@gmail.com>
2022-11-06 08:23:21 +0530

    +-/*%** using function and use of return statement

---
+++ python026.py
@@ -0,0 +1,26 @@
+#costum function we can use it fo any mathematical calculation
+def add(num1,num2):
+    sum = num1 + num2
+    print(sum)
+add(5,10)
+add(3343234542525235,823746285623865)
+def subtract(num3,num4):
+    sub = num3 - num4
+    print(sub)
+subtract(123456987, 987456123)
+subtract(42, 32)
+def multiplication(num5,num6):
+    mul = num5 * num6
+    print(mul)
+multiplication(45, 35)
+def division(num7,num8):
+    div = num7 // num8
+    print(div)
+division(144,12)
+def square(num9,num0):
+    sqr = num9 ** num0
+    return sqr
+res = square(5, 5)
+print(res)
+# use of return statement
+# Instructions that a Python interpreter can execute are called statements

32a36c18352a924b7c426243c1d0f45a329e1c1b
Kriti <kritijulia@gmail.com>
2022-11-06 08:26:46 +0530

    determing global and local def function

---
+++ python027.py
@@ -0,0 +1,14 @@
+def name(myName):
+    print("my Name is :" + myName )
+name("xyz")
+def Name(firstname,lastname):
+    print("my Name is :" + firstname + "" + lastname )
+Name("kriti","julia")
+#firstName has already have a assinged value vishal so when the pranthesis is empty it will be automatically vishal
+def Name1(firstName = "Vishal"):
+    print("my Name is :" + firstName)
+Name1("kriti")
+Name1("knjkiti")
+Name1("kdfiti")
+Name1()
+Name1("krdsti")

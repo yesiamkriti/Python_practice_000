97c1f08f9156015fb12b3231691d3a071839f77e
Kriti <kritijulia@gmail.com>
2022-11-06 08:18:08 +0530

    functon introduction

---
+++ python024.py
@@ -0,0 +1,10 @@
+#function introduction
+def hello():
+    print("hello world")
+    
+def hello_mars():
+    print("hello mars")
+hello()
+hello_mars()
+# def is used to define a function
+# every when we have to create a new function we can do the same as above

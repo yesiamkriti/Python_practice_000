9c36f905ef5f726578cff51ce96754746ac631ef
Kriti <kritijulia@gmail.com>
2022-11-06 08:19:51 +0530

    Display Fibonacci series up to 10 terms

---
+++ python021.py
@@ -0,0 +1,16 @@
+'''Display Fibonacci series up to 10 terms The Fibonacci Sequence is a series of numbers.
+The next number is found by adding up the two numbers before it. The first two numbers are 0 and 1.
+For example, 0, 1, 1, 2, 3, 5, 8, 13, 21. 
+The next number in this series above is 13+21=34. Expected output:
+Fibonacci sequence: 0 1 1 2 3 5 8 13 21 34'''
+a=int(input("Enter a number:"))
+n1,n2= 0,1
+sum = 0
+if a<=0:
+    print('enter grater number:')
+else:
+    for i in range(0,a):
+        print(sum,end="")
+        n1= n2
+        n2= sum
+        sum= n1 + n2

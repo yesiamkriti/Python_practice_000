21e38f1f25cebc4c9e6fae2f6ad2cfd7442dd2d5
Kriti <kritijulia@gmail.com>
2022-11-06 08:34:06 +0530

    use of * and recursion

---
+++ python029.py
@@ -0,0 +1,12 @@
+def details(name,**data):
+    print(name)
+    print(data)
+"""for i,j in data.items():
+    print(i,j)"""
+details("Kriti",place="Gaya",age=19,mob_no=9982387235)
+a = 10
+def fun():
+    a=15
+    print(a)
+fun()
+print(a)

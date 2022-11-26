394b078be4dad447070611c9e3b866d7f50dc8dc
Kriti <kritijulia@gmail.com>
2022-11-26 15:04:22 +0530

    def add,mult,sub,div

---
+++ python034.py
@@ -0,0 +1,13 @@
+a=int(input("num1:"))
+b=int(input("num2:"))
+c=str(input("operator:"))
+def num(a,b):
+    if c=="+":
+        print(a+b)
+    if c=="-":
+        print(a-b)
+    if c=="/":
+        print(a/b)
+    if c=="*":
+        print(a*b)
+num(a,b)

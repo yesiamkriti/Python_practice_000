95c6518ddaee0b094de759d91b67cf752069c9b4
Kriti <kritijulia@gmail.com>
2022-11-28 11:45:53 +0530

    special characters

---
+++ python040.py
@@ -0,0 +1,17 @@
+# print the following line
+# this is \\ duoble backslash
+# these are /\/\/\/\/\ mountains
+# he is     aswesome (use escape sequesnce insted of manual space)
+# "\n \t \' (print this as an output)
+print("print \\\\ double blackslash")
+print("these are /\\/\\/\\/\\/\\ mountains")
+print("he is "" aswesome")
+print("he is \t awesome")
+print("\""'\\n \\t \\'"'")
+print("\\\"\\n \\t \\\'")
+print("\"")
+print("\\")
+print("\\\"")
+# \" = "
+# \\ = \
+# \\\" = \"

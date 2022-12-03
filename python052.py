7e9a09fc3ea518618eab9395102f92275cfdba8e
Kriti <kritijulia@gmail.com>
2022-12-03 13:57:35 +0530

    use of set

---
+++ python052.py
@@ -0,0 +1,24 @@
+# strore multiple  values in single variable
+# unordered
+# unchangeable
+# duplicates are not allowed
+myset = {"bike","car","boat"}
+myset5 = {"apple","mango","banana"}
+myset1 = {1,2,3,4}
+myset4 = {4,5,6}
+myset2 = myset.union(myset1)
+myset.add("cycle")
+myset.remove("bike")
+print(myset)
+print(myset2)
+print(myset1.union(myset4))
+print(myset1.intersection(myset4))
+print(myset1.symmetric_difference(myset4))
+print(myset1.issuperset(myset4))
+myset.symmetric_difference_update(myset5)
+print(myset)
+mydictionary = {
+    "name":"Nikhil",
+    "age":"22"
+}
+print(mydictionary)

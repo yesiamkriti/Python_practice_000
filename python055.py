6e18604050a14a12082e3ad9110d68c0194cb376
Kriti <kritijulia@gmail.com>
2022-12-05 18:53:07 +0530

    nested dicitionary

---
+++ python055.py
@@ -0,0 +1,12 @@
+dict1 = {
+    "class":{
+        "student":{
+            "name":"xyz",
+            "marks":{
+                "python":100,
+                "web":90
+            }
+        }
+    }
+}
+print(dict1["class"]["student"]["marks"]["python"])

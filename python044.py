400b8e80460d98e842711328c58cabf5ec3fa614
Kriti <114799895+yesiamkriti@users.noreply.github.com>
2022-12-08 10:13:11 +0530

    Delete python044.py

--- python044.py
+++
@@ -1,8 +0,0 @@
-#taking input of different variables at a time
-# use of split ... if we write .split() we have to give space after entering 1st input
-# if we write .split(",") then we have use comma after each input
-name, age, gender = input("Enter yoyr name, age and gender ").split(",")
-print(name, age, gender)
-print(name)
-print(age)
-print(gender)

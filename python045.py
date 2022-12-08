6a7c5b41ef33baad908524104492ce8310e4af27
Kriti <114799895+yesiamkriti@users.noreply.github.com>
2022-12-08 10:13:33 +0530

    Delete python045.py

--- python045.py
+++
@@ -1,9 +0,0 @@
-# string formatting
-name = "kriti"
-age = 19
-# print("hello"+name+"your age is"+str(age)) # ugly syntax
-# in python3 wde use as
-print("hello {} your age is {} ".format(name,age))
-# in python3.6
-print(f"hello {name} your age is {age} ")
-print(f"hello {name} your age is {age -5} ")# we can also do calculation in it

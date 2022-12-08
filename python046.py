21f89fd9e7844aef402c70011c3b70b245ab4c52
Kriti <114799895+yesiamkriti@users.noreply.github.com>
2022-12-08 10:18:30 +0530

    Delete python046.py

--- python046.py
+++
@@ -1,5 +0,0 @@
-# ask user to input 3 numbers and you hav e to print average of threee numbers using strinh formatting
-# bonus :- try to take all three comma seperated inputs in one line
-# taking input in one line
-a,b,c= input("enter three number: ").split(",")
-print(f"average of three numbers is :{(int(a) + int(b) + int(c))/3}")

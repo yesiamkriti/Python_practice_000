c18e9909b09f15a43d8b62ffe50b7797a2a11346
kriti <kritijulia@gmail.com>
2022-12-08 09:57:00 +0530

    counting repetition of chracter of string

--- python061.py
+++ python061.py
@@ -1,5 +1,8 @@
 n = str(input())
+temp_var =""
 p=0
 while p< len(n):
-    print(f"{n[p]} :{n.count(n[p])}")
+    if n[p] not in temp_var:
+        temp_var += n[p]
+        print(f"{n[p]} :{n.count(n[p])}")
     p+=1

063662e5aa1045917fab0ec329fe39ba150171fd
kriti <kritijulia@gmail.com>
2022-12-09 23:38:49 +0530

    different way to write a program

---
+++ python065.py
@@ -0,0 +1,11 @@
+# different way to write a program
+name = (input("Enter your name: "))
+for i in range(len(name)): # this lang is also used in java
+    print(name[i])
+for j in name : # in python
+    print(j)
+number = (input("enter a number: "))
+total = 0 
+for s in number:
+    total += int(s)
+print(total)

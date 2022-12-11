b9a911170b40006534a9f26f41273bd82a60bd78
kriti <kritijulia@gmail.com>
2022-12-11 19:33:57 +0530

    split and join method in list

---
+++ python074.py
@@ -0,0 +1,9 @@
+# split method
+# convert string to list
+user_info = "kriti,19 ".split(",")
+print(user_info)# it will split the string and conver into ['kriti', '19']
+
+#join method
+# convert list into string
+user_info2 = ['kriti','19']
+print(','.join(user_info2))

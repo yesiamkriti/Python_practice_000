6323ee300e119ba61dade6d502de3c3ab4344f68
kriti <kritijulia@gmail.com>
2022-12-13 08:23:22 +0530

    exercise reverse child elemen in a list

---
+++ python078.py
@@ -0,0 +1,11 @@
+# define a function that take list of words as argument and return list with reverse of every element in that list
+# example
+# -----> ['cba', 'vut','zyx']
+def list1(l):
+    a=[]
+    for x in b:
+        a.append(x[::-1])
+    return a
+
+b=['abc', 'tuv','xyz']
+print(list1(b))

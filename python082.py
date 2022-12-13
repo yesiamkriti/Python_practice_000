7599759388e5c7f05a9e64667244d62ffd05d187
kriti <kritijulia@gmail.com>
2022-12-13 08:26:55 +0530

    exercise no of child element in a list

---
+++ python082.py
@@ -0,0 +1,13 @@
+# def function
+# [1,2,3,[1,2],[3,4]]----->input
+# no. of elements in child list ------>output
+# def fun(list1):
+#     count = 0
+#     for i in list1 :
+#         if type(i)== list:
+#             count += 1
+#     return count
+# mixed = [1,2,3,[1,2],[3,4]]
+# print(fun(mixed))
+mixed = [1,2,3,[1,2],[3,4]]
+print(count(mixed))

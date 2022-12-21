142d5ccb79c5728f6625eb943d567a2de4c04847
kriti <kritijulia@gmail.com>
2022-12-22 00:30:02 +0530

    exercise on list comprehension

---
+++ python098.py
@@ -0,0 +1,7 @@
+# define a function that takes list of strings
+# list containing reverse of every string 
+# NOTE - use list comprehension because we already did this exercise using normal method
+# exampe l =['abc','tuv','xyz']
+# reverse_string(l)-----> ['cba','vut','zyx']
+print([i[::-1] for i in ['abc','tuv','xyz']])
+

b9621ff8ca8588783e2dcc2c8954a43299df3c41
kriti <kritijulia@gmail.com>
2022-12-24 16:54:16 +0530

    adding line in file

---
+++ python101.py
@@ -0,0 +1,7 @@
+introduction = open("introduction.txt","w")
+print(introduction.write("Hello! Everyone , I am Kriti .Today I am learning python.\n"))
+# introduction = open("introduction.txt","a")
+print(introduction.write(" In python sir is teaching us the last chapter which is file.\n "))
+print(introduction.write(" In file we are learning how to add file,write in file and many more"))
+# gc garbage collector
+

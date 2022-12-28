756b34865bc1ba541187a2cba6222772489e6fdb
kriti <kritijulia@gmail.com>
2022-12-29 00:10:19 +0530

    import regex and literal raw

--- python104.py
+++ python104.py
@@ -4,4 +4,14 @@ import re
 # output = re.search("^This.**string$",value)
 # print(output)
 pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=-])(?=.*{8,})"
-print(pattern)
+# this is known as regex....
+# r is a literal raw
+# ^ ---- this is known as circumflex . it is used to start a string in regex from
+# ()--- this is used to define a type of container 
+# ? --- question mark is use for zero and one occurance
+# . ----- dot is use for any character (except newline character)
+# * ----- is use for zero and one occurence
+# []------ sqare bracket is used as container or set
+
+# -    this is used to define range
+# updated

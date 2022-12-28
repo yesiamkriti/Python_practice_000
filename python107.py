76aff0b13c06448e336e811c48d9da411c2ca950
kriti <kritijulia@gmail.com>
2022-12-29 00:08:17 +0530

    exercise for practice

--- python107.py
+++ python107.py
@@ -7,6 +7,10 @@ def num(l):
     return[str(i) for i in l if type(i) == int or type(i)==float]
 print(num([True, False,[1,2,3],1,1.0,3]))
 
+
+# or
+
+
 list2=[]
 def list1():
     for x in [True, False,[1,2,3],1,1.0,3]:
@@ -14,3 +18,4 @@ def list1():
             list2.append(str(x))
     print(list2) 
 list1()
+# updated

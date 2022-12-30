b7a702caf722c2368a4941b39b86f0569dcf5e15
kriti <kritijulia@gmail.com>
2022-12-31 03:57:41 +0530

    * args

---
+++ python113.py
@@ -0,0 +1,12 @@
+# make flexible function
+# *operator
+#  *args
+def total(a,b):
+    return a+b
+print(total(45,7,4,45,34,2,34,545,3,5,35,53,5,5,34,26))
+def all_total(*args):
+    total = 0
+    for num in args:
+        total=total+num
+    return total
+print(all_total(1,2,4,6,7,8,9,6,1,2,25,45,))

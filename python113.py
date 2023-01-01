59e0c806685d916034a69cf299b2cbc55c7911ed
kriti <kritijulia@gmail.com>
2023-01-01 12:49:53 +0530

    * argument packing

--- python113.py
+++ python113.py
@@ -3,7 +3,7 @@
 #  *args
 def total(a,b):
     return a+b
-print(total(45,7,4,45,34,2,34,545,3,5,35,53,5,5,34,26))
+print(total(45,7,4,45,34,2,34,545,3,5,35,53,5,5,34,26)) # here we cannot put more than two value so this is showing synatax error
 def all_total(*args):
     total = 0
     for num in args:

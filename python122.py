df31cd2c14e7bf8766be3cc69eed1fd6a3c8948e
kriti <kritijulia@gmail.com>
2023-01-03 12:41:55 +0530

    zip function

---
+++ python122.py
@@ -0,0 +1,7 @@
+# zip function 
+# ziping means packing of two or more data . it is iterator 
+user_id = ['user1','user2','user3']
+names = ['harshit','mohit','rohit']
+state = ['Bihar','Jharkhand','Odissa']
+print(zip(user_id,names,state)) # this is proving that zip is iterator
+print(list(zip(user_id,names,state)))

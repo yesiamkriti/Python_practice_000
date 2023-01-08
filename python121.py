5dc1e9da92f23774111ac8e7cd0321f4a9c523ad
kriti <kritijulia@gmail.com>
2023-01-08 15:40:27 +0530

    iterartir vs iterables

--- python121.py
+++ python121.py
@@ -4,5 +4,6 @@ squares = map(lambda a: a**2,numbers)#iterator
 # for i in numbers:
 #     print(i)
 print(next(iter(numbers)))
-print(list(iter(numbers)))
 print(next(squares))
+# number is iterable so we have to convert it in iterator first then it became iterator 
+# if fun is iterator then just apply next funtion it will print the reqired answer

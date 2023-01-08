017b8cd87be8782fad611a96b9d76f5ba7d5e6ad
kriti <kritijulia@gmail.com>
2023-01-08 15:38:21 +0530

    advance max and min function

--- python123.py
+++ python123.py
@@ -2,6 +2,8 @@ l = [(1,2),(4,3),(5,6),(7,8)]
 print(list(zip(*l)))
 l1, l2 = list(zip(*l))
 print(list(l1),list(l2))
+# print the max number from l3 and l2 by comparing the same index number
+# eg 4> 2 print 4 ....and so on ...[4, 8, 7, 2, 3]
 l2 = [4,5,3,1,2]
 l3 = [2,8,7,2,3]
 l4 = []

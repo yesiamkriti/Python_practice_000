fdb5626a31000f27f44e525756134d3eb4cb0245
kriti <kritijulia@gmail.com>
2023-01-10 12:21:54 +0530

    doc string,write ,see docstring, help function

---
+++ python131.py
@@ -0,0 +1,14 @@
+# what are doc strings
+# how to write docstring 
+# how to seee docstring 
+# what is help function
+
+def add(a,b):
+    '''this function takes two number and return their sum'''
+    return a+b
+print(add(56,35))
+print(add.__doc__)
+print(len.__doc__)
+print(sum.__doc__)
+print(max.__doc__)
+print(help(sum))
